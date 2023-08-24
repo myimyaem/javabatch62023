9c84e2aa6c3c5094e3098fe927ce47ec7fcad56d
myimyaem <137817981+myimyaem@users.noreply.github.com>
2023-08-24 08:54:57 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,23 @@
+import java.util.Random;
+
+public class Main {
+  public static void main(String[] marco) {
+    Random r = new Random();
+    System.out.println("the random generated number is: "+r.nextInt(50));
+    String name = "marco"; //variable
+    greetName(name); //use greetName method
+    greetName("Grace");
+  }
+  // method sample
+ public static void greetName(String name){
+   System.out.println("Hello "+name);
+  
+  }
+}
+
+class Drinks{
+  
+}
+class Snack{
+  
+}
---
+++ Store.java
@@ -0,0 +1,11 @@
+public class Store{
+  
+}
+
+class Cashier{
+  
+}
+
+class Items{
+  
+}
